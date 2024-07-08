0f4c3421931197990dd9a1372726a996f18cc392
Kaan Kani İnce <kkince@newmind.ai>
2024-07-08 16:16:15 +0300

    custom-tabs created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TabTest from "./components/custom-tabs/tab-test";
+
+function App() {
+  return (
+    <div className="App">
+        {/* Custom tabs component */}
+      <TabTest/>
+    </div>
+  );
+}
+
+export default App;
